03257c52e36dbfe9148ed4acb62a410a80236e60
caleb <calebtelfe@gmail.com>
2025-06-17 14:05:10 -0400

    caesar cipher done.

--- javascript.js
+++ javascript.js
@@ -10,4 +10,47 @@ function reverseString(str) {
     }
     return reversed;
 }
-  module.exports = {capitalize, reverseString};
+
+const calculator = {
+    add: function (a, b) {
+        return a+b;
+    },
+
+    subtract: function (a, b) {
+        return a-b;
+    },
+
+    multiply: function (a, b) {
+        return a*b;
+    },
+
+    divide: function (a, b) {
+        return a/b;
+    }
+}
+
+
+function caesarCipher(str, shiftFactor) {
+    //quick google search... ASCII 97-122 = a-b. 65-90 = A-Z
+    let currentChar = "";
+    let charCode = 0;
+    let encodedStr = "";
+    for (let i = 0; i < str.length; i++) {
+        currentChar = str[i];
+        charCode = currentChar.charCodeAt(0);
+
+        if (charCode >=97 && charCode <= 122) { //lowercase
+            let shiftedCode = ((charCode - 97 + shiftFactor) %26) + 97;
+            encodedStr += String.fromCharCode(shiftedCode);
+
+        } else if (charCode >= 65 && charCode <= 90) {
+            let shiftedCode = ((charCode - 65 + shiftFactor) %26) + 65;
+            encodedStr += String.fromCharCode(shiftedCode);
+        } else {
+            encodedStr += String.fromCharCode(charCode);
+        }
+        
+    }
+    return encodedStr;
+}
+  module.exports = {capitalize, reverseString, calculator, caesarCipher};
--- javascript.test.js
+++ javascript.test.js
@@ -1,4 +1,4 @@
-const {capitalize,  reverseString} = require('./javascript.js');
+const {capitalize,  reverseString, calculator, caesarCipher} = require('./javascript.js');
 
 test('String to uppercase first char', () => {
   expect(capitalize("canada")).toBe("Canada");
@@ -7,3 +7,30 @@ test('String to uppercase first char', () => {
 test('Reverses a String', () => {
     expect(reverseString("apple")).toBe("elppa");
   });
+
+
+
+test("Add two numbers", () => {
+    expect(calculator.add(1,2)).toBe(3);
+})
+
+test("minus two numbers", () => {
+    expect(calculator.subtract(2,2)).toBe(0);
+})
+
+test("mult two numbers", () => {
+    expect(calculator.multiply(1,2)).toBe(2);
+})
+
+test("divide two numbers", () => {
+    expect(calculator.divide(1,2)).toBe(0.5);
+})
+
+
+test("Caesar cipher wrapping", () => {
+    expect(caesarCipher("xyz", 3)).toBe("abc");
+})
+
+test("Caesar cipher punctuation", () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+})
